80aa966da3168d236ff6c8962e77de9d3b638946
Denylson Melo <denylsonmelo@gmail.com>
2017-12-13 09:31:33 -0300

    adicionando page detalhes

--- CloneFlix/src/app/app.component.ts
+++ CloneFlix/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { DetalhePage } from '../pages/detalhe/detalhe';
 import { HomePage } from '../pages/home/home';
-import { PerfisPage } from '../pages/perfis/perfis';
 
 @Component({
   templateUrl: 'app.html'
--- CloneFlix/src/app/app.module.ts
+++ CloneFlix/src/app/app.module.ts
@@ -8,27 +8,30 @@ import { MyApp } from './app.component';
 
 import { PerfisPage } from '../pages/perfis/perfis';
 import { HomePage } from '../pages/home/home';
+import { DetalhePage } from '../pages/detalhe/detalhe';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    PerfisPage,
-	HomePage
-],
-imports: [
-	BrowserModule,
-    IonicModule.forRoot(MyApp)
-],
-bootstrap: [IonicApp],
-entryComponents: [
-	MyApp,
-	PerfisPage,
-	HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+	declarations: [
+		MyApp,
+		PerfisPage,
+		HomePage,
+		DetalhePage
+	],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		PerfisPage,
+		HomePage,
+		DetalhePage
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{ provide: ErrorHandler, useClass: IonicErrorHandler }
+	]
 })
-export class AppModule {}
+export class AppModule { }
