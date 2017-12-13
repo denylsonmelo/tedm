cdea05e4d66cf7c653bc23f8a0035de995842b79
Denylson Melo <denylsonmelo@gmail.com>
2017-12-13 10:25:56 -0300

    adicionando bloqueio para a tela ficar em portrait

--- CloneFlix/src/app/app.component.ts
+++ CloneFlix/src/app/app.component.ts
@@ -5,20 +5,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { DetalhePage } from '../pages/detalhe/detalhe';
 import { HomePage } from '../pages/home/home';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+	rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, screenOrientation: ScreenOrientation) {
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			statusBar.styleDefault();
+			splashScreen.hide();
+			screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT);
+		});
+	}
 }
 
--- CloneFlix/src/app/app.module.ts
+++ CloneFlix/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 import { MyApp } from './app.component';
 
@@ -31,6 +32,7 @@ import { DetalhePage } from '../pages/detalhe/detalhe';
 	providers: [
 		StatusBar,
 		SplashScreen,
+		ScreenOrientation,
 		{ provide: ErrorHandler, useClass: IonicErrorHandler }
 	]
 })
