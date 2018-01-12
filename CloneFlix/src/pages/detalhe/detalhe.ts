4833542fc03288a7c06e8afee1f8671a20752eec
Denylson Melo <denylsonmelo@gmail.com>
2018-01-12 12:14:24 -0300

    tentando personalizar o fab de classificacao e adicionar o select de temporadas

--- CloneFlix/src/pages/detalhe/detalhe.ts
+++ CloneFlix/src/pages/detalhe/detalhe.ts
@@ -15,7 +15,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetalhePage {
 
-	video: { nome: string, ano: number, classificaoEtaria: number, ehFilme: boolean, duracao: string, temporadas: string, qualidade: string, resumo: string, elenco: Array<string>, criacao: Array<string>, jaEstaClassificado: boolean, classificaoBoa: boolean } = { nome: "Stranger Things", ano: 2016, classificaoEtaria: 16, ehFilme: false, duracao: "", temporadas: "2 temporadas", qualidade: 'hd', resumo: "No caminho de volta para casa, Will é aterrorizado por alguma coisa. Não longe dali, um laboratório secreto guarda um segredo sinistro.", elenco: ["Winona Ryder", "David Harbour"], criacao: ["The Duffer Brothers"], jaEstaClassificado: true, classificaoBoa: false };
+	temporadaEscolhida: string;
+
+	video: { nome: string, ano: number, classificaoEtaria: number, ehFilme: boolean, duracao: string, temporadas: string, qualidade: string, resumo: string, elenco: Array<string>, criacao: Array<string>, jaEstaClassificado: boolean, classificaoBoa: boolean, estahMinhaLista: boolean, nomeTemporadas: Array<string> } = { nome: "Stranger Things", ano: 2016, classificaoEtaria: 16, ehFilme: false, duracao: "", temporadas: "2 temporadas", qualidade: 'hd', resumo: "No caminho de volta para casa, Will é aterrorizado por alguma coisa. Não longe dali, um laboratório secreto guarda um segredo sinistro.", elenco: ["Winona Ryder", "David Harbour"], criacao: ["The Duffer Brothers"], jaEstaClassificado: true, classificaoBoa: false, estahMinhaLista: false, nomeTemporadas: ["Temporada 1", "Temporada 2", "Temporada 3"] };
 
 	constructor(public navCtrl: NavController, public navParams: NavParams) {
 	}
